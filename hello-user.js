6df703791f0ced6c58355d50aaf88dea63e67f4b
masha-a-m <escritoramaree@gmail.com>
2024-01-07 21:03:01 +0800

    edited categories, new arrival pages

--- hello-user.js
+++
@@ -1,7 +0,0 @@
-// Translate to a specific language
-function translateTo(language) {
-    google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-    var selectElement = document.querySelector(".goog-te-combo");
-    selectElement.value = language;
-    selectElement.dispatchEvent(new Event('change'));
-}
